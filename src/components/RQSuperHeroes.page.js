2d766bb6f1817f982a3def3e354e23be13c2df0a
boong <taurusboong2@gmail.com>
2022-07-08 23:41:32 +0900

    RQ data transformation

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -4,16 +4,20 @@ import { RQfetchHeroes } from '../networks/api';
 
 export const RQSuperHeroesPage = () => {
   const onSuccess = data => {
-    console.log('Perform side effect after data fetching', data);
+    console.log('Perform side effect after data fetching');
   };
 
   const onError = error => {
-    console.log('Perform side effect after encounting error', error);
+    console.log('Perform side effect after encounting error');
   };
 
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery('super-heroes', RQfetchHeroes, {
     onSuccess,
     onError,
+    select: data => {
+      const superHeroesNames = data.data.map(hero => hero.name);
+      return superHeroesNames;
+    },
   });
 
   if (isLoading || isFetching) return <h2>Loading...</h2>;
@@ -22,8 +26,11 @@ export const RQSuperHeroesPage = () => {
     <>
       <h2>React Query Super Heroes Page</h2>
       <button onClick={refetch}>Fetch Heroes</button>
-      {data?.data.map(hero => {
+      {/* {data?.data.map(hero => {
         return <div key={hero.id}>{hero.name}</div>;
+      })} */}
+      {data.map(heroName => {
+        return <div key={heroName}>{heroName}</div>;
       })}
     </>
   );
