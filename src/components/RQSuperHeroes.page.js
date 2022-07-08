971d7ece3fc310c3f1038e1d398c9a3f9c8e9d81
boong <taurusboong2@gmail.com>
2022-07-08 23:19:36 +0900

    RQ btn click refetching

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -3,16 +3,16 @@ import { useQuery } from 'react-query';
 import { RQfetchHeroes } from '../networks/api';
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data, isError, error, isFetching } = useQuery('super-heroes', RQfetchHeroes, {
-    refetchInterval: 2000,
-    refetchIntervalInBackground: true,
+  const { isLoading, data, isError, error, isFetching, refetch } = useQuery('super-heroes', RQfetchHeroes, {
+    enabled: false,
   });
 
-  if (isLoading) return <h2>Loading...</h2>;
+  if (isLoading || isFetching) return <h2>Loading...</h2>;
   if (isError) return <h2>{error.message}</h2>;
   return (
     <>
       <h2>React Query Super Heroes Page</h2>
+      <button onClick={refetch}>Fetch Heroes</button>
       {data?.data.map(hero => {
         return <div key={hero.id}>{hero.name}</div>;
       })}
